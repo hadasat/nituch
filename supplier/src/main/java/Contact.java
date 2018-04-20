ed182523d79d8161433ca5f1e73e957572f28e09
hadasat <atiyah@post.bgu.ac.il>
2018-04-20 19:10:34 +0300

    f

--- supplier/src/main/java/Contact.java
+++ supplier/src/main/java/Contact.java
@@ -7,4 +7,14 @@ public class Contact {
     String lastName;
     String phoneNumber;
     String email;
+
+    Contact(String supplierId, String firstName, String lastName, String phoneNumber,String email){
+        this.supplierId = supplierId;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
 }
