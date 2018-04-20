4af48de5a7d5827046de3a86c215384525f332dd
hadasat <atiyah@post.bgu.ac.il>
2018-04-20 18:20:43 +0300

    n

--- supplier/src/main/java/UserConsole.java
+++ supplier/src/main/java/UserConsole.java
@@ -4,6 +4,11 @@
 public class UserConsole {
 
     public static void main(String[] args){
-
+        System.out.println("please select an action by choosing the index: \n" +
+                "1.add new supplier \n" +
+                "2.add new contact for supplier \n" +
+                "3.show discounts \n" +
+                "4.order report by supplier\n" +
+                "");
     }
 }
