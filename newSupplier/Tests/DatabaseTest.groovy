b5df995daab1503d07e07d9e0e162d9c5b6e505f
hadasat <atiyah@post.bgu.ac.il>
2018-04-23 11:24:21 +0300

    מקווה שזה לא ימחוק דברים חשובים

---
+++ newSupplier/Tests/DatabaseTest.groovy
@@ -0,0 +1,57 @@
+class DatabaseTest{
+
+    void testAdd_Supplier() {
+        int supplierId = 1235
+        int bankAccount = 1234
+        String payment = check
+        String supplyForm = "Mon"
+        Supplier supplier = new Supplier(supplierId,bankAccount,payment,supplyForm)
+        assert "Add supplier succeeded" == Add_Supplier(supplier)
+    }
+
+    void testUpdateSupplier() {
+
+    }
+
+    void testSelect_supplier() {
+    }
+
+    void testAdd_item() {
+    }
+
+    void testUpdateItem() {
+    }
+
+    void testSelect_Item() {
+    }
+
+    void testAdd_order() {
+    }
+
+    void testUpdateOrder() {
+    }
+
+    void testSelect_Order() {
+    }
+
+    void testSelect_Not_Recived_Orders() {
+    }
+
+    void testAdd_Discount() {
+    }
+
+    void testUpdateDiscount() {
+    }
+
+    void testSelect_Discount() {
+    }
+
+    void testAdd_Contact() {
+    }
+
+    void testUpdateContact() {
+    }
+
+    void testSelect_All_Contact_of_supplier() {
+    }
+}
